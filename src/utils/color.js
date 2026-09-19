ce2a04b3cd0b7d30b83b39f64ed10ba69d9ed1ba
sebadorn <sebadorn+git@posteo.de>
2026-09-19 20:03:29 +0200

    feat(utils): add "color" util

---
+++ src/utils/color.js
@@ -0,0 +1,44 @@
+/**
+ *
+ * @param {Color} c
+ * @returns {string} The color has 8 character hex value, e.g. "#a0550cef".
+ */
+export function colorToHex( c ) {
+	const r = Math.round( c.r * 255 ).toString( 16 ).padStart( 2, '0' );
+	const g = Math.round( c.g * 255 ).toString( 16 ).padStart( 2, '0' );
+	const b = Math.round( c.b * 255 ).toString( 16 ).padStart( 2, '0' );
+	const a = Math.round( c.a * 255 ).toString( 16 ).padStart( 2, '0' );
+
+	return `#${r}${g}${b}${a}`;
+};
+
+
+/**
+ *
+ * @param {string} c
+ * @returns {Color}
+ */
+export function hexToColor( c ) {
+	if( c[0] === '#' ) {
+		c = c.slice( 1 );
+	}
+
+	const len = c.length === 3 ? 1 : 2;
+	const hasAlpha = c.length === 4 || c.length === 8;
+
+	return {
+		r: parseInt( c.substring( 0, len ), 16 ) / 255,
+		g: parseInt( c.substring( len, len ), 16 ) / 255,
+		b: parseInt( c.substring( len + len, len ), 16 ) / 255,
+		a: hasAlpha ? parseInt( c.substring( len + len + len, len ), 16 ) / 255 : 1,
+	};
+};
+
+
+/**
+ * @typedef {Object} Color
+ * @property {number} r Red value as [0, 1].
+ * @property {number} g Green value as [0, 1].
+ * @property {number} b Blue value as [0, 1].
+ * @property {number} a Alpha value as [0, 1].
+ */
