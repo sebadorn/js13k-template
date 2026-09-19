1f5184f8b752f807cff5c83376b8266e890a3682
sebadorn <sebadorn+git@posteo.de>
2026-09-19 22:18:55 +0200

    chore: add eslint.config.mjs and replace "@stylistic/eslint-plugin-js" with "@stylistic/eslint-plugin"

---
+++ eslint.config.mjs
@@ -0,0 +1,66 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+
+
+export default [
+	js.configs.recommended,
+	{
+		ignores: [
+			'build/',
+			'node_modules/',
+		],
+	},
+	{
+		files: [
+			'src/**/*.js',
+		],
+		languageOptions: {
+			sourceType: 'module',
+		},
+	},
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+			},
+		},
+		plugins: {
+			'@stylistic': stylistic,
+		},
+		rules: {
+			'@stylistic/indent': [
+				'error',
+				'tab',
+				{
+					SwitchCase: 1,
+				},
+			],
+			'@stylistic/linebreak-style': [
+				'error',
+				'unix',
+			],
+			'@stylistic/no-empty': 0,
+			'no-sparse-arrays': 0,
+			'no-unused-vars': [
+				'warn',
+				{
+					argsIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+					caughtErrorsIgnorePattern: '^_',
+				},
+			],
+			'no-var': ['error'],
+			'@stylistic/one-var-declaration-per-line': ['warn', 'always'],
+			'@stylistic/quotes': [
+				'error',
+				'single',
+				{
+					avoidEscape: true,
+				},
+			],
+			'@stylistic/semi': ['error', 'always'],
+			'@stylistic/space-in-parens': ['warn', 'always'],
+		},
+	},
+];
